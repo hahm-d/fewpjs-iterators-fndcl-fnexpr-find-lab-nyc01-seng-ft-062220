e2ec48ba13abe36b4e57a14f4a8dc3bce16b7f4d
hahm-d <dhahm01@gmail.com>
2020-08-06 21:34:02 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arrObj){
+  let output = arrObj.find(({result}) => {
+    return result === "W"
+  })
+  return !!output ? output.year : undefined
+}
+
+
+
+const sampledata = [{ year: "1968", result: "N/A"},
+{ year: "1967", result: "N/A"},
+{ year: "1961", result: "N/A"},
+{ year: "1960", result: "N/A"}];
+
+console.log(superbowlWin(sampledata))
